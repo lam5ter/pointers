5f97be77a002b190f23d3978fcf2f2d19b74089b
Ben Lam <lam5ter@users.noreply.github.com>
2017-07-08 21:50:48 +1000

    Add files via upload

--- pointers.c
+++ pointers.c
@@ -1,3 +1,8 @@
+//Pointers
+//Written by Nathan Lam, 8/7/17
+//For the full code, go to:
+//https://github.com/lam5ter/pointers
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,12 +20,10 @@ int main(int argc, char *argv[]) {
     printf("*p = %d\n", *p);   
     printf("p[0] = %d\n", p[0]);
 
-    printf("\n\b The following code is extension work.\n");
-
+    printf("\nBelow shows how pointers can be used with arrays.\n");
     printf("Size of INT datatype: %lu\n", sizeof(int));
     int a[ARRAY_SIZE];      //assign a pointer
-
-    int i;                  //Iterator
+    int i;                  //iterator
     int *q = a;
     //Set 5 consecutive locations in memory to
     //0, 1, 2, 3, 4 respectively.
@@ -32,6 +35,9 @@ int main(int argc, char *argv[]) {
     //for (i = 0; i < ARRAY_SIZE; i++) a[i] = i;
     for (i = 0; i < ARRAY_SIZE; i++)
         printf("At the address %p -> a[%d] = %d\n", &a[i], i, a[i]);
+    //Notice that addresses differ by the size of an int datatype
 
     return EXIT_SUCCESS;
+
 }
+
