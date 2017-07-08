acfdc0f9a70a50eda359bfafa4d6755335d78c50
Ben Lam <lam5ter@users.noreply.github.com>
2017-07-08 21:56:43 +1000

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
         *q = i;
         q++;
     }
-    //Lines 31-34 can be replaced by the following line
+    //Lines 32-35 can be replaced by the following line
     //for (i = 0; i < ARRAY_SIZE; i++) a[i] = i;
     for (i = 0; i < ARRAY_SIZE; i++)
         printf("At the address %p -> a[%d] = %d\n", &a[i], i, a[i]);
